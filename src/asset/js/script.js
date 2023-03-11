f808b4175b204559854c073436ad5f1440fe84e6
S-Miyakawa <seiji.miya@nifty.com>
2023-03-11 14:32:24 +0900

    breadcrumb追記

--- src/asset/js/script.js
+++ src/asset/js/script.js
@@ -35,6 +35,10 @@ window.onload = function(){
 		data = JSON.parse(JSON.stringify(data));
 
 		let fileName = window.location.href.split('/').pop().replace('.html','');
+		if(fileName == ""){
+			let urls = window.location.href.split('/');
+			fileName = urls[urls.length -2];
+		}
 
 		if(fileName != "index" && fileName !== ""){
 			let bread = document.querySelector('.bread');
@@ -43,12 +47,12 @@ window.onload = function(){
 	
 			data.forEach(el => {
 				if(el.name === fileName){
-					code = '<ul>';
+					code = '<ul class="bread__ul">';
 	
 					for(let i = 0; i<el.text.length -1 ;i++){
-						code += '<li><a href="' + el.url[i] + '">' + el.text[i] + '</a></li>';
+						code += '<li class="bread__li"><a class="bread__link" href="' + el.url[i] + '">' + el.text[i] + '</a></li>';
 					}
-					code += `<li>${el.text[el.text.length-1]}</li></ul>`;
+					code += `<li class="bread__li">${el.text[el.text.length-1]}</li></ul>`;
 				}
 	
 				bread.innerHTML = code;
